7dc5d156e654535a96b251bd085e461aa9e21b1d
Lucas dos Santos Craveiro <lucas.dos.santos.craveiro@gmail.com>
2023-12-04 03:02:08 -0300

    Continuando assinatura

--- resources/views/assinatura.blade.php
+++ resources/views/assinatura.blade.php
@@ -42,42 +42,46 @@
             <p class="fs-5 ">O cliente deseja assinar digitalmente?</p>
             <p>Para assinar o seu documento, precisamos revisar todas as informações.</p>
         </div>
-        <div>
-            <object class="col-8" data="http://localhost/Compass/criadorPDF" type="application/pdf"></object>
+        <div class="d-flex justify-content-center localPDF">
+            <object class="col-10" data="http://localhost/Compass/criadorPDF" type="application/pdf"></object>
         </div>
-        <div>
-            <p>Todas as informações estão corretas? </p>
-            <button id="sim" onclick='exibeRepresentante()'
-                class="btn-compass-color border border-0 rounded-4 px-4 =">Sim</button>
-            <button id="nao" onclick='errado()'
-                class="btn-compass-color border border-0 rounded-4 px-4 =">Não</button>
+        <div class="col-12">
+            <div class="d-flex flex-column align-items-center col-12">
+                <p>Todas as informações estão corretas? </p>
+                <div class="d-flex justify-content-evenly col-6">
+                    <button id="sim" onclick='exibeRepresentante()' class="col-5 btn-compass-color border border-0 rounded-4 px-4 =">Sim</button>
+                    <button id="nao" onclick='errado()' class="col-5 btn-compass-color border border-0 rounded-4 px-4 =">Não</button>
+                </div>
+            </div>
             <div id="exibicao" class="d-none">
                 <p class="pt-5">Insira as informações necessárias para assinar sua proposta comercial de acordo.</p>
                 <div>
                     <form method="GET" action="/pegarValorAssinante">
-                        <div class="d-flex me-5">
-                            <div>
-                                <label label for="nome">Nome completo</label>
-                                <input name="nomeAssinante" id="nomeAssinante" type="text"
-                                    class="rounded-4 py-1" placeholder="João da Silva">
-                            </div>
-                            <div>
-                                <label for="documento">Documento de identidade (CPF)</label>
-                                <input name="docAssinante" id="docAssinante" type="text" class="rounded-4 py-1"
-                                    placeholder="000.000.000-00">
-                            </div>
-                            <div>
-                                <label for="email">Email</label>
-                                <input name="emailAssinante" id="emailAssinante" type="text"
-                                    class="rounded-4 py-1" placeholder="João.Representante@email.com">
-                            </div>
-                            <div>
-                                <label for="email">Token fornecido no Email</label>
-                                <input name="token" id="token" type="text"
-                                    class="rounded-4 py-1" placeholder="000001">
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="d-flex col-10">
+                                <div class="col-3 pe-2">
+                                    <label label for="nome">Nome completo</label>
+                                    <input name="nomeAssinante" id="nomeAssinante" type="text"
+                                        class="form-control" placeholder="João da Silva">
+                                </div>
+                                <div class="col-3 pe-1">
+                                    <label for="documento">Documento de identidade (CPF)</label>
+                                    <input name="docAssinante" id="docAssinante" type="text" class="form-control"
+                                        placeholder="000.000.000-00">
+                                </div>
+                                <div class="col-3 ps-1">
+                                    <label for="email">Email</label>
+                                    <input name="emailAssinante" id="emailAssinante" type="text"
+                                        class="form-control" placeholder="João.Representante@email.com">
+                                </div>
+                                <div class="col-3 ps-2">
+                                    <label for="email">Token fornecido no Email</label>
+                                    <input name="token" id="token" type="text"
+                                        class="form-control" placeholder="000001">
+                                </div>
                             </div>
                             <button name='envio' value='envio' id='envio'
-                                class="btn-compass-color border border-0 rounded-4">Assinar</button>
+                                class="col-3 btn-compass-color border border-0 rounded-4 mt-3">Assinar</button>
                         </div>
                     </form>
                 </div>
