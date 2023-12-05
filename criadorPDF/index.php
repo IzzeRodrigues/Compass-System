63a9dae9b1928fb150dbd15441114d5d6c3c1079
Izze Rodrigues <isap4350@gmail.com>
2023-12-05 13:50:23 -0300

    comentado

--- criadorPDF/index.php
+++ criadorPDF/index.php
@@ -3741,7 +3741,8 @@ function assinando($variaveis)
 
             date_default_timezone_set('America/Sao_Paulo');
             $hora= date('G:i:s');
-
+            
+            // que porra é essa????????
             $token = "";
           
             if ($ip_assinante != 'UNKNOWN')
